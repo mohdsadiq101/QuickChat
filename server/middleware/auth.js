6b3a0b5e9dc0498e11da97c9f5acb4059c9e8df1
Mohd Sadique <mohdsadique757@gmail.com>
2025-08-26 15:06:27 +0530

    included JWT token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,14 +4,16 @@ import jwt from "jsonwebtoken";
 //Middleware to protect routes
 export const protectRoute = async (req, res, next) => {
     try {
-        const token = req.headers.token;
-
+        let token = req.headers.token;
+        if (!token && req.headers.authorization) {
+            token = req.headers.authorization.replace('Bearer ', '');
+        }
+        if (!token) {
+            return res.json({ success: false, message: "jwt must be provided" });
+        }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
         const user = await User.findById(decoded.userId).select("-password");
-
         if(!user) return res.json({ success: false, message: "User not found" });
-
         req.user = user;
         next();
     } catch (error) {
